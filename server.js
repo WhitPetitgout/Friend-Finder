c99d616af24948020899135ab3954ac2c5cb9366
Whit <whit.danger@gmail.com>
2018-03-15 14:42:11 -0600

    new bugs

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
-require('./app/routing/api-routes')(app); 
-require('./app/routing/html-routes')(app);
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
 
 
 app.listen(PORT, function () {
